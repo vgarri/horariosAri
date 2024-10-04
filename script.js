eec13bea3e5dfa6a6cf6ec412fda98d7527cb158
Victor Garritano <victor@getupgraded.com>
2024-10-05 00:16:24 +0200

    Terminado

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
-
-
 document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let horas=prompt("Introduce el total de horas (La suma total)");
-    let minutos=prompt("introduce la suma de minutos por ejemplo:(30+41...etc )");
-function sumadorHoras(horas, minutos){
-    let suma = 40 - horas + (minutos*(1/60));
+  event.preventDefault();
+  let horas = prompt("Introduce el total de horas (La suma total)");
+  let minutos = prompt("introduce la suma de minutos por ejemplo:(30...etc )");
+  function sumadorHoras(horas, minutos) {
+
+    let suma = 40 - horas + minutos * (1 / 60);
+    
     return suma;
-}
-alert(`Te faltan por hacer ${sumadorHoras(horas, minutos)} horas :(`);
+  }
+  let suma2 = sumadorHoras(horas, minutos);
+  //alert(`Te faltan por hacer ${Math.round(suma2)} horas :(`);
+    if (suma2 < 0) {
+        alert(`Esta semana te pasaste ${Math.abs(suma2)} horas... deberías descansar >:(`);
+      }
+      else if (suma2 === 0) {
+        alert(`Ya llevas 40 horitas siuuuuu ^o^`);
+      }
+      else if (suma2 > 0) {
+        alert(`Todavía te faltan ${Math.trunc(suma2)} horas y ${(60-minutos).toFixed(0)} mins :(`)
+      }
+      
+
+
 });
