0408e3fdefc97d3caa3dc445a0ec5176df5e3740
Victor Garritano <victor@getupgraded.com>
2024-10-05 10:56:23 +0200

    v0.3

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ document.querySelector("form").addEventListener("submit", function (event) {
   let minutos = prompt("introduce la suma de minutos por ejemplo:(30...etc )");
   function sumadorHoras(horas, minutos) {
 
-    let suma = 40 - horas + minutos * (1 / 60);
+    let suma = (40 - horas + minutos * (1 / 60)).toFixed(2);
     
     return suma;
   }
+  let sumaseparada = [];
   let suma2 = sumadorHoras(horas, minutos);
-  //alert(`Te faltan por hacer ${Math.round(suma2)} horas :(`);
+  sumaseparada.push(suma2.toString().split("."));
+  
     if (suma2 < 0) {
-        alert(`Esta semana te pasaste ${Math.abs(suma2)} horas... deberías descansar >:(`);
+        alert(`Esta semana te pasaste ${(Math.abs(sumaseparada[0]))} horas...deberías descansar >:(`);
       }
-      else if (suma2 === 0) {
+      else if (suma2==0) {
         alert(`Ya llevas 40 horitas siuuuuu ^o^`);
       }
       else if (suma2 > 0) {
-        alert(`Todavía te faltan ${Math.trunc(suma2)} horas y ${(60-minutos).toFixed(0)} mins :(`)
+        alert(`Todavía te faltan ${sumaseparada[0]} horas :(`)
       }
       
 
